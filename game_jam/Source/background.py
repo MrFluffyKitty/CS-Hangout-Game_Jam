ff4f44ee60f556c54688431571a2214335e206bb
Austin Little <austinlittle2014@gmail.com>
2021-12-20 23:03:11 -0600

    Deleted bg file

--- game_jam/Source/background.py
+++
@@ -1,6 +0,0 @@
-import pygame
-import os
-
-# Loads background image and sets it to screen dimensions
-# BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join(
-# 'game_jam', 'Assets', 'Background', 'Galaxy_bg', 'Purple_Nebula', 'PN1.png')), (700, 700)).convert()
